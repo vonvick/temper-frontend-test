d92d70eb7db70ce1c4036f22da8ef1e6a95ad0fa
Victor Nwaiwu <victor.nwaiwu@andela.com>
2019-11-30 17:26:03 +0100

    create action list and action items components

---
+++ src/components/ActionItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <CardComponent>
+    <div class="flex h-12 items-center justify-between">
+      <div>
+        <p>{{ description }}</p>
+      </div>
+      <div>
+        <Button
+          v-on:handle-click-event="$emit('perform-time-travel', actionIndex)"
+          :class="{
+            rounded: true,
+            'text-black': true,
+            'bg-green-400': true
+          }"
+        >
+          Time Travel
+        </Button>
+      </div>
+    </div>
+  </CardComponent>
+</template>
+<script>
+import CardComponent from "@/components/utility/CardComponent";
+import Button from "@/components/utility/Button";
+
+export default {
+  name: "ActionItem",
+  components: {
+    CardComponent,
+    Button
+  },
+  props: {
+    description: String,
+    actionIndex: Number
+  },
+  data() {
+    return {};
+  }
+};
+</script>
